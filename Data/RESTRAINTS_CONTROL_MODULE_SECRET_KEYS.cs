0554ddec1b9e39eed41fb9e4c94118a6a761d6c6
Jack Leighton <jakka351@outlook.com>
2023-08-03 14:00:54 +1000

    Create RESTRAINTS_CONTROL_MODULE_SECRET_KEYS.cs
    yippee kay yay

---
+++ Data/RESTRAINTS_CONTROL_MODULE_SECRET_KEYS.cs
@@ -0,0 +1,11 @@
+        // ///////////////////////////////////////////////////////////////////////////////////////
+        //             Service 0x27 securityAccess Secret Keys for Restraints Control Module
+        //         
+        public class RCM_SECURITY_ACCESS_KEYS
+            : object {
+            public object rcmSecretKeyLevelOne = 0x44494F4445;
+            public object rcmSecretKeyLevelOneAscii = "DIODE";
+            public object rcmSecretKeyLevelTwo = 0x526F77616E;
+            public object rcmSecretKeyLevelTwoAscii = "Rowan"; 
+            public object systemSupplierSpecificRCMKey = 0x00;
+        }
