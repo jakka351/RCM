d72b9a3e1d6483f84cd841be8b77cb17c4705aec
Jack Leighton <jakka351@outlook.com>
2023-08-03 14:25:49 +1000

    Create RESTRAINTS_CONTROL_MODULE_DTC.cs

---
+++ Data/RESTRAINTS_CONTROL_MODULE_DTC.cs
@@ -0,0 +1,15 @@
+B2292 RCM   Restraint System - Safety Belt Pretensioner Status (Driver or Passenger Pretensioner Circuit Fault)            
+B2293 RCM   Restraint System - Airbag Status (Driver or Passenger Front Airbag Circuit Fault)            
+B2295 RCM   Restraint System - Side Airbag Module Status (Driver or Passenger Side Side Airbag Circuit Fault)           
+B2296 RCM   Restraint System - Impact Sensor Status (Front, Driver or Passenger Side Front Row Impact Severity Sensor Circuit Fault)            
+B2434 RCM   Driver Safety Belt Buckle Switch Circuit Short To Ground          
+B2435 RCM   Driver Safety Belt Buckle Switch Circuit Resistance Out Of Range           
+B2438 RCM   Passenger Safety Belt Buckle Switch Circuit Short to Ground          
+B2439 RCM   Passenger Safety Belt Buckle Switch Circuit Resistance Out of Range           
+B2477 Park, BEM, RCM Module Configuration Failure           
+B2691 RCM   Driver Safety Belt Buckle Switch Circuit Fault           
+B2692 RCM   Passenger Safety Belt Buckle Switch Circuit Fault           
+C1414 RCM   Incorrect Vehicle ID          
+C1947 RCM   Seat Track Position Sensor Circuit Short to Ground          
+C1948 RCM   Seat Track Position Sensor Circuit Resistance Out of Range           
+C1981 RCM   Seat Track Position Sensor Circuit Fault           
