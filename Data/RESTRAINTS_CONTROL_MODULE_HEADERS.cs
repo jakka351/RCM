a815f383ecdb85603f64569ae571fcf4e8dd3e8e
Jack Leighton <jakka351@outlook.com>
2023-08-03 14:02:38 +1000

    Create RESTRAINTS_CONTROL_MODULE_HEADERS.cs

---
+++ Data/RESTRAINTS_CONTROL_MODULE_HEADERS.cs
@@ -0,0 +1,26 @@
+    // /////////////////////////////////////////////////////////////////////////////////////
+    //     Restraints Control Module
+    //     CAN Diagnostic Identifier RX & TX for Restraints Control Module
+    //     ata Identifier Memory Locations
+    //     For Crash Data Retrieval
+    //     
+    public class RESTRAINTS_CONTROL_MODULE
+        : object {
+        // 
+        //             CAN Diagnostic Identifier RX & TX for Restraints Control Module
+        //         
+        public class RCM_DIAGSIG_RX_TX_IDENTIFIER
+            : object {
+            
+            public object RCM_DiagSig_Rx = 0x737;
+            public object RCM_DiagSig_Tx = 0x73F;
+        }
+        // 
+        //             CAN Diagnostic Identifier RX & TX for Restraints Control Module
+        //         
+        public class RCM_RAPID_DATA_IDENTIFIERS
+            : object {
+            
+            public object RCM_DiagSig_Rx = 0x737;
+            public object RCM_DiagSig_Tx = 0x73F;
+        }
